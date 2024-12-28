1febfc9930c27651fbd7d6b8fd7790cdeb2ab3ae
Afia <afiakhan166@gmail.com>
2024-12-28 23:42:18 +0500

    Create skyshare with vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import AppRouter from "./config/routes";
+import "./index.css"
+
+function App() {
+  return (
+    <AppRouter />
+  )
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import AppNavbar from "../../components/Navbar";
+import "../Home/css/style.scss"
+
+const HomePage = () => {
+    return(
+        <div className="main">
+        {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1> */}
+        <AppNavbar />
+        </div>
+    )
+} 
+
+export default HomePage;
