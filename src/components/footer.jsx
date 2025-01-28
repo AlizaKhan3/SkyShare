c4404bef9a1d296472a79f0cca6a363038c541da
Aliza Khan <alizaadi2@gmail.com>
2025-01-29 00:08:20 +0500

    edited footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,20 @@
 import { PiHeartFill } from "react-icons/pi";
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const AppFooter = () => {
-    return(
-        <div className="text-center">
-        <div class="flex justify-center items-center gap-1" style={{marginTop:"70px"}}>
-                    <p>Made by Aliza Khan with  </p>
-                    <PiHeartFill style={{color:"red"}} />
+    return (
+        // <div className="text-center">
+        <div class="flex justify-center items-center gap-2 mb-4" style={{ marginTop: "70px", color:"#0D3A71", fontWeight:"bold" }}>
+            {/* <p>Made by Aliza Khan </p> */}
+            {/* <PiHeartFill style={{color:"red"}} /> */}
+            <p>Follow me on</p>
+            <div class="flex justify-center items-center gap-2" >
+                <a href="https://github.com/AlizaKhan3" target="_blank"><FaGithub className="h-10" /></a>
+                <a href="https://www.linkedin.com/in/aliza-khan3/" target="_blank"><FaLinkedin /></a>
+            </div>
         </div>
-        <p>Follow me on  </p>
-        </div>
+        // </div>
 
     )
 }
--- src/db/index.js
+++ src/db/index.js
@@ -4,26 +4,6 @@ import { getDatabase, ref, set, onValue, remove } from "firebase/database";
 import { getFirestore, collection, addDoc, getDoc, getDocs} from "firebase/firestore";
 import { getStorage, ref as storageRef, uploadBytesResumable, getDownloadURL } from 'firebase/storage'; // Correct import
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDUef_wqO6cD5qcJQBiXXO8Oo6Md1vOK2c",
-//     authDomain: "skyy-share.firebaseapp.com",
-//     projectId: "skyy-share",
-//     storageBucket: "skyy-share.firebasestorage.app",
-//     messagingSenderId: "407752094853",
-//     appId: "1:407752094853:web:ccec24896337453a00a5ec",
-//     databaseURL: "https://skyy-share-default-rtdb.asia-southeast1.firebasedatabase.app",
-// };
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDzpnkFF0DiZ32MVAUaMiTEu83A7QAUnx8",
-    authDomain: "onlinefood-7799a.firebaseapp.com",
-    databaseURL: "https://onlinefood-7799a-default-rtdb.firebaseio.com",
-    projectId: "onlinefood-7799a",
-    storageBucket: "onlinefood-7799a.appspot.com",
-    messagingSenderId: "119235898205",
-    appId: "1:119235898205:web:9a6ca0302608a92a7556b4",
-    databaseURL: "https://onlinefood-7799a-default-rtdb.firebaseio.com"
-}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
